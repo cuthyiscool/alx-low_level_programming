ee0165967ed075a19f1df87bf7139cc3d62c7e58
cuthyiscool <cuthbertn15@gmail.com>
2023-10-04 15:02:26 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,7 +7,6 @@
  *
  * Return: the int converted from the string
  */
-
 int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
@@ -21,30 +20,29 @@ int _atoi(char *s)
 
 	while (s[len] != '\0')
 		len++;
+
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
 			++d;
 
 		if (s[i] >= '0' && s[i] <= '9')
-
 		{
 			digit = s[i] - '0';
 			if (d % 2)
 				digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
-		if (s[i + 1] < '0' || s[i + 1] > '9')
-}
+			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
-
 			f = 0;
 		}
 		i++;
 	}
-	if (f == 0)
 
+	if (f == 0)
 		return (0);
+
 	return (n);
 }
 
@@ -55,10 +53,11 @@ int _atoi(char *s)
  *
  * Return: 0 (Success), 1 (Error)
  */
-
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
+	int result, num1, num2;
+
+	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
 		return (1);
@@ -67,6 +66,7 @@ int main(int argc, char *argv[])
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
 	result = num1 * num2;
+
 	printf("%d\n", result);
 
 	return (0);
